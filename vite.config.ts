1f83ed27b773118262d83409e9d43e20637c5d80
devSaifur <saifur2akash@gmail.com>
2024-11-29 02:20:38 +0600

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [solid()],
-});
+  plugins: [react()],
+})

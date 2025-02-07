5acd156536f461049404f0c270c8a62aec64436f
devSaifur <saifur2akash@gmail.com>
2025-02-08 01:16:32 +0600

    setup vercel deploy

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
-import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
 import { csrf } from 'hono/csrf'
 import { logger } from 'hono/logger'
+import { handle } from 'hono/vercel'
 
 import { authMiddleware } from './middleware'
 import { authRoutes } from './routes/auth-routes'
 import { notesRoutes } from './routes/notes-routes'
+
+export const runtime = 'edge'
 
 const app = new Hono()
 
@@ -21,8 +22,6 @@ app.use(csrf())
 
 app.use(authMiddleware)
 
-app.use('*', serveStatic({ root: './dist' }))
-
 app.onError((err, c) => {
   console.dir(`Error: ${err.message}`, { colors: true })
   return c.json({ error: 'Something went wrong' }, 500)
@@ -31,9 +30,7 @@ app.onError((err, c) => {
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
-serve({
-  fetch: app.fetch,
-  port
-})
+export const GET = handle(app)
+export const POST = handle(app)
 
 export type ApiType = typeof apiRoutes
